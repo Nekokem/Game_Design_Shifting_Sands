02d9bf7d2981c2e95b507deebfb1a1a092aafa10
Katie Morley <kamytherabbit@gmail.com>
2020-01-31 16:02:55 -0700

    started the whitebox and added a character controller

---
+++ Shifting Sands/Assets/scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    private Vector3 position;
+    private CharacterController controller;
+
+    public int speed = 10;
+    public float gravity = 10.0f;
+    public int jumpSpeed = 20;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        position.x = speed * Input.GetAxis("Horizontal");
+        position.z = speed * Input.GetAxis("Vertical");
+        position.z -= gravity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            position.y = jumpSpeed;
+        }
+
+        controller.Move(position * Time.deltaTime);
+    }
+    
+}
