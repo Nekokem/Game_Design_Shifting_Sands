244f04e98dc695eeaab313bf788a621f94af0fa5
Katie Morley <kamytherabbit@gmail.com>
2020-02-13 16:01:22 -0700

    Added magic scripts and triggerevent script

---
+++ Shifting Sands/Assets/scripts/TriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent, TriggerExitEvent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        TriggerExitEvent.Invoke();
+    }
+}
---
+++ Shifting Sands/Assets/scripts/UseMagic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class UseMagic : MonoBehaviour
+{
+    public ParticleSystem Spell;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Spell.Emit(25);
+        }
+    }
+}
