9e41463c8559fc15467dc9d7db6bf6c21e551a4f
Katie Morley <kamytherabbit@gmail.com>
2020-02-18 17:09:32 -0700

    Added character mechanics

---
+++ Shifting Sands/Assets/scripts/CharacterCombat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterCombat : MonoBehaviour
+{
+    public float attackSpeed = 1f;
+    private float attackCoolDown = 0f; //slows down the attacks to once per frame so the enemy doesn't die too quickly
+    public float attackDelay = 0f;
+    
+    private CharacterStats myStats;
+
+    public event System.Action OnAttack;
+    void Start()
+    {
+        myStats = GetComponent<CharacterStats>();
+    }
+
+    private void Update()
+    {
+        attackCoolDown -= Time.deltaTime;
+    }
+
+    public void Attack(CharacterStats targetStats)
+    {
+        if (attackCoolDown <= 0f)
+        {
+            StartCoroutine(DoDamage(targetStats, attackDelay ));
+            if (OnAttack != null)
+                OnAttack();
+            
+            attackCoolDown = 1f / attackSpeed;
+        }
+       
+    }
+
+    IEnumerator DoDamage(CharacterStats stats, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        stats.TakeDamage(myStats.damage.GetValue());
+        
+    }
+}
---
+++ Shifting Sands/Assets/scripts/CharacterStats.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHP = 100;
+    private int currentHP { get; set; }
+    
+    public Stats damage;
+
+    public HealthBar healthbar;
+
+    //public FloatData damage;
+    
+    public Color hitColor = Color.red;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+    }
+    private void Start()
+    {
+        if (healthbar != null) healthbar.SetMaxHealth(maxHP);
+    }
+
+    private void Update()
+    {
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage = Mathf.Clamp(damage, 0, int.MaxValue); //damage can never go below 0
+        
+        currentHP -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        GetComponent<Renderer>().material.color = hitColor;
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+        healthbar.SetHealth(currentHP);
+    }
+    
+    public virtual void Die()
+    {
+        //die in some way, run animation, start back at starting position, etc.
+        //this will later be changed depending on who is dying
+        Debug.Log(transform.name + " died.");
+    }
+}
