58087a5aa1f40769404d088f37825080e58fa887
Katie Morley <kamytherabbit@gmail.com>
2020-02-19 21:55:40 -0700

    Enemy scripts most

---
+++ Shifting Sands/Assets/scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//handles interaction with the Enemy
+
+[RequireComponent(typeof(EnemyStats))]
+[RequireComponent(typeof(EnemyController))]
+[RequireComponent(typeof(CharacterCombat))]
+
+public class Enemy : Interactable
+{
+    private PlayerManager playerManager;
+    private CharacterStats myStats; //referencing the stats of the enemy
+    
+    public Color restingColor;
+    
+    private void Start()
+    {
+        playerManager = PlayerManager.instance;
+        myStats = GetComponent<CharacterStats>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        CharacterCombat playerCombat = playerManager.player.GetComponent<CharacterCombat>();
+
+        if (playerCombat != null)
+        {
+            playerCombat.Attack(myStats);
+            Debug.Log("you are attacking the enemy");
+            
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        gameObject.GetComponent<Renderer>().material.color = restingColor;
+    }
+
+    
+}
