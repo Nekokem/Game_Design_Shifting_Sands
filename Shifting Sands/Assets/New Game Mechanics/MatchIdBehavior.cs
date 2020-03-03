160374f1357b757d5d5d6265ac4c79184c67a22a
Katie Morley <kamytherabbit@gmail.com>
2020-03-02 21:00:22 -0700

    Started the matching system
    added matchidbehavior

---
+++ Shifting Sands/Assets/New Game Mechanics/MatchIdBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchIDBehavior : BehaviorID
+{
+    public WorkSystemManager workSystemManagerObj;
+    private NameId otherIdObj;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        otherIdObj = other.GetComponent<BehaviorID>().nameIDObj;
+        CheckId();
+    }
+
+    private void CheckId()
+    {
+        foreach (var obj in workSystemManagerObj.workIdList)
+        {
+            
+            if (otherIdObj == obj.nameIdObj)
+            {
+                obj.workSystemObj.Work();
+            }
+        }
+    }
+
+}
