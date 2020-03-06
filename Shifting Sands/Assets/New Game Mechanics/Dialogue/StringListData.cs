2f3d943ab454ad75983938f12c41f9e55305d73b
Katie Morley <kamytherabbit@gmail.com>
2020-03-05 21:09:27 -0700

    added stringlistdata

---
+++ Shifting Sands/Assets/New Game Mechanics/Dialogue/StringListData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StringListData : ScriptableObject
+{
+    public List<string> stringListObj;
+    public int currentLineNumber;
+
+    public string ReturnCurrentLine()
+    {
+        return stringListObj[currentLineNumber];
+    }
+
+    public void SwitchToNextLine()
+    {
+        if (currentLineNumber < stringListObj.Count-1)
+        {
+            currentLineNumber++;
+        }
+        else
+        {
+            currentLineNumber = 0;
+        }
+    }
+}
