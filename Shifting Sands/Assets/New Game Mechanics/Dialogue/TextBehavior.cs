fe4a79bfe31ad26bdb8f963d443be3545ed94ee0
Katie Morley <kamytherabbit@gmail.com>
2020-03-09 22:02:06 -0600

    Added text behavior script

---
+++ Shifting Sands/Assets/New Game Mechanics/Dialogue/TextBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehavior : MonoBehaviour
+{
+
+    private Text textObj;
+
+    private void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        textObj.text = StringListData.ReturnCurrentLine();
+    }
+}
