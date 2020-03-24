239fb2f7b21e19715f8dfeb85a96879b26fd82ac
Katie Morley <kamytherabbit@gmail.com>
2020-03-24 15:24:50 -0600

    Added intData and game action scripts

--- Shifting Sands/Assets/New Game Mechanics/Dialogue/TextBehavior.cs
+++ Shifting Sands/Assets/New Game Mechanics/Dialogue/TextBehavior.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
-
-[RequireComponent(typeof(Text))]
-public class TextBehavior : MonoBehaviour
-{
-
-    private Text textObj;
-
-    private void Awake()
-    {
-        textObj = GetComponent<Text>();
-    }
-
-    void Update()
-    {
-        textObj.text = StringListData.ReturnCurrentLine();
-    }
+﻿using UnityEngine; 
+using UnityEngine.Events; 
+using UnityEngine.UI; 
+ 
+[RequireComponent(typeof(Text))] 
+public class TextBehavior : MonoBehaviour 
+{ 
+     
+    private Text textObj; 
+    public UnityEvent awakeEvent; 
+     
+    void Awake() 
+    { 
+        textObj = GetComponent<Text>(); 
+    } 
+     
+    public void UpdateText(StringListData stringListDataObj) 
+    { 
+        textObj.text = stringListDataObj.ReturnCurrentLine(); 
+    } 
+    /*Cant use this in event system!*/ 
+ 
+    public void UpdateText(IntData intDataObj) 
+    { 
+        textObj.text = intDataObj.value.ToString(); 
+    } 
+  
 }
---
+++ Shifting Sands/Assets/New Game Mechanics/Game Actions/GameActionHandler.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+
+
+    public GameAction gameActionObj;
+    public UnityEvent handlerEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameActionObj.action += Action;
+    }
+
+    private void Action()
+    {
+    
+        handlerEvent.Invoke();
+        
+    }
+}
