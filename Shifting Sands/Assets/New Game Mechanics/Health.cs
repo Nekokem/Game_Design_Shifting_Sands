2214a9b52fdc70f1bcae6b2fdfd88d9ecc08a098
Katie Morley <kamytherabbit@gmail.com>
2020-03-25 22:38:58 -0600

    added health script

---
+++ Shifting Sands/Assets/New Game Mechanics/Health.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public int healthCollection = 100;
+    public UnityEvent TakeDamageEvent;
+    public UnityEvent DieEvent;
+
+    public void CrocodileSubtractHeath()
+    {
+        healthCollection -= 10;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+
+    public void CobraSubtractHealth()
+    {
+        healthCollection -= 5;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+    
+    public void KingCcbraSubtractHealth()
+    {
+        healthCollection -= 20;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+    
+    public void MummySubtractHealth()
+    {
+        healthCollection -= 10;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+    
+    public void PharaohSubtractHealth()
+    {
+        healthCollection -= 25;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+
+    public void SahSubtractHealth()
+    {
+        healthCollection -= 30;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+
+    public void GainHealth()
+    {
+        healthCollection += 20;
+    }
+
+    public void Died()
+    {
+        DieEvent.Invoke();
+    }
+}
