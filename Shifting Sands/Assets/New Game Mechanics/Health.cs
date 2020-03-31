d77bb09a48210943c0c495ff5dfed36fdeed02d5
Katie Morley <kamytherabbit@gmail.com>
2020-03-31 14:02:17 -0600

    Edited health and added mouse down event script

--- Shifting Sands/Assets/New Game Mechanics/Health.cs
+++ Shifting Sands/Assets/New Game Mechanics/Health.cs
@@ -9,40 +9,7 @@ public class Health : MonoBehaviour
     public UnityEvent TakeDamageEvent;
     public UnityEvent DieEvent;
 
-    public void CrocodileSubtractHeath()
-    {
-        healthCollection -= 10;
-        TakeDamageEvent.Invoke();
-
-        if (healthCollection == 0)
-        {
-            Died();
-        }
-    }
-
-    public void CobraSubtractHealth()
-    {
-        healthCollection -= 5;
-        TakeDamageEvent.Invoke();
-
-        if (healthCollection == 0)
-        {
-            Died();
-        }
-    }
-    
-    public void KingCcbraSubtractHealth()
-    {
-        healthCollection -= 20;
-        TakeDamageEvent.Invoke();
-
-        if (healthCollection == 0)
-        {
-            Died();
-        }
-    }
-    
-    public void MummySubtractHealth()
+    public void SubtractHealth(int attackDamage)
     {
         healthCollection -= 10;
         TakeDamageEvent.Invoke();
@@ -53,28 +20,6 @@ public class Health : MonoBehaviour
         }
     }
     
-    public void PharaohSubtractHealth()
-    {
-        healthCollection -= 25;
-        TakeDamageEvent.Invoke();
-
-        if (healthCollection == 0)
-        {
-            Died();
-        }
-    }
-
-    public void SahSubtractHealth()
-    {
-        healthCollection -= 30;
-        TakeDamageEvent.Invoke();
-
-        if (healthCollection == 0)
-        {
-            Died();
-        }
-    }
-
     public void GainHealth()
     {
         healthCollection += 20;
---
+++ Shifting Sands/Assets/New Game Mechanics/MouseDownEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class MouseEvents : MonoBehaviour, IPointerDownHandler
+{
+    public UnityEvent mouseDownClick;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        mouseDownClick.Invoke();
+    }
+}
