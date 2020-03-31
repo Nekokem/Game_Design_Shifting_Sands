a0fc603f6d17f7b73400ddc5699908b27208ef45
Katie Morley <kamytherabbit@gmail.com>
2020-03-31 14:02:28 -0600

    Added player attack

---
+++ Shifting Sands/Assets/New Game Mechanics/PlayerAttack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public GameObject Obj;
+   
+    private void Start()
+    {
+        
+    }
+
+    public void ChangeMaterial()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}
