1d50c7fbbfb667eb9b62398b59f6ee9996227aa1
Katie Morley <kamytherabbit@gmail.com>
2020-03-31 14:01:36 -0600

    Added damage trigger script

---
+++ Shifting Sands/Assets/New Game Mechanics/DamageTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+    public int AttackDamage;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>())
+        {
+            other.GetComponent<Health>().SubtractHealth(AttackDamage);
+        }
+    }
+}
