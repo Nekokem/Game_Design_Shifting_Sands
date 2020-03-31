975b1034295b724a9741f8de9154512fb35c3e86
Katie Morley <kamytherabbit@gmail.com>
2020-03-31 14:01:25 -0600

    Added attack script

---
+++ Shifting Sands/Assets/New Game Mechanics/Attack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject hitbox;
+    public float Timer;
+    public bool AttackingIsRunning = false;
+    public int AttackingHealth;
+
+    void Update()
+    {
+        //get input
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(AttackingIsRunning == false){
+                GetComponent<Health>().SubtractHealth(AttackingHealth);
+                StartCoroutine(Attacking());
+            }
+        }      
+    }
+
+    IEnumerator Attacking()
+    {
+        AttackingIsRunning = true;
+
+        //turn on hitbox
+        hitbox.SetActive(true);
+        
+        //Set timer
+        yield return new WaitForSeconds(Timer); 
+
+        //turn off hitbox
+        hitbox.SetActive(false);
+
+        AttackingIsRunning = false;
+    }
+}
