b413bb33d3347be23f58a74fb2f5a841303334f1
Katie Morley <kamytherabbit@gmail.com>
2020-04-01 18:06:30 -0600

    Added Enemy health script

---
+++ Shifting Sands/Assets/New Game Mechanics/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float healthCollection = 5f;
+    public UnityEvent TakeDamageEvent;
+    public UnityEvent DieEvent;
+
+    public void MinusHealth(float playAttackDamage)
+    {
+        healthCollection -= 10;
+        TakeDamageEvent.Invoke();
+
+        if (healthCollection == 0)
+        {
+            Died();
+        }
+    }
+    
+    public void Died()
+    {
+        DieEvent.Invoke();
+    }
+}
