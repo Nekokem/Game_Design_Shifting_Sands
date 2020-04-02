178f5a2efc9c53afb7a527e33cb1c6260ab15641
Katie Morley <kamytherabbit@gmail.com>
2020-04-01 18:09:04 -0600

    Added mousedown script

---
+++ Shifting Sands/Assets/New Game Mechanics/MouseDownEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class MouseDownEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownClick;
+    public UnityEvent mouseUpEvent;
+
+    public void OnMouseDown()
+    {
+        mouseDownClick.Invoke();
+    }
+
+    public void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+}
