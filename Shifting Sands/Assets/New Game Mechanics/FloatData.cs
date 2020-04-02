db85ace786c87f609a119b8fa17492a0a850f9b5
Katie Morley <kamytherabbit@gmail.com>
2020-04-01 18:06:50 -0600

    Edited FloatData script

---
+++ Shifting Sands/Assets/New Game Mechanics/FloatData.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+    public float maxValue = 1f;
+
+    public void UpdateVale(float amount)
+    {
+
+        value += amount;
+
+    }
+
+    public void UpdateValueLimitZero(float amount)
+    {
+
+        if (value < 0)
+        {
+            value = 0;
+        }
+
+        else
+        {
+            UpdateVale(amount);
+        }
+    }
+
+    public void UpdateValueLimitZeroAndMaxValue(float amount)
+    {
+
+        if (value < maxValue)
+        {
+            UpdateVale(amount);
+        }
+        else
+        {
+            value = maxValue;
+        }
+        UpdateValueLimitZero(amount);
+
+    }
+
+    public void changeValue(float amount)
+    {
+        value = amount;
+    }
+
+}
