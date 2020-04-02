9ffd6c49b695108bf479ba614a566ab761603414
Katie Morley <kamytherabbit@gmail.com>
2020-04-01 18:09:31 -0600

    Added playerdamage script

---
+++ Shifting Sands/Assets/New Game Mechanics/PlayerDamageTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageTrigger : MonoBehaviour
+{
+    public int PlayerAttackDamage;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Worked???");
+        /*if (other.GetComponent<EnemyHealth>())
+        {
+            other.GetComponent<EnemyHealth>().MinusHealth(AttackDamage);
+        }*/
+    }
+}
