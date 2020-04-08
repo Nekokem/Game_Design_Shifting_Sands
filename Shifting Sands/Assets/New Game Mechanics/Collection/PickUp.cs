76a438eca35f9e6e40aec90c295e6d03340b5f1d
Katie Morley <kamytherabbit@gmail.com>
2020-04-07 21:21:41 -0600

    made pick up script

---
+++ Shifting Sands/Assets/New Game Mechanics/Collection/PickUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+  public Inentory inventory;
+  public pickUpType thisPickUp;
+  public enum pickUpType
+  {
+    GEM,
+    Health,
+    MAGIC
+  }
+
+  public void OnTriggerEnter(Collider other)
+  {
+    if (thisPickUp == pickUpType.GEM)
+    {
+      inventory.AddGem();
+    }
+    
+    Destroy(gameObject);
+  }
+}
