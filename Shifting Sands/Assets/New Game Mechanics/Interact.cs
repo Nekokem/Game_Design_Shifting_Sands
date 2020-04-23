4f9ada42f02562442127d6490033e25720e642b7
Katie Morley <kamytherabbit@gmail.com>
2020-04-23 16:40:13 -0600

    interact scripts

---
+++ Shifting Sands/Assets/New Game Mechanics/Interact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public bool canInteract = false;
+    
+        public GameObject interactableObj;
+        public GameObject popUpBox;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Interact")
+            {
+                popUpBox.SetActive(true);
+                interactableObj = other.gameObject;
+                canInteract = true;
+            }
+        }
+    
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Interact")
+            {
+                popUpBox.SetActive(false);
+                canInteract = false;
+            }
+        }
+        
+        public void Update()
+        {
+            if (canInteract && Input.GetKeyDown(KeyCode.E))
+            {
+                interactableObj.GetComponent<InteractEvent>().interactEvent.Invoke();
+            }
+        }
+}
