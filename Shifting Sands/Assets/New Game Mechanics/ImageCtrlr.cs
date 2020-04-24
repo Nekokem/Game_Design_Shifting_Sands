8b2f7822d551e1a210d5066d6903ebf3379ab3e6
Katie Morley <kamytherabbit@gmail.com>
2020-04-24 11:15:12 -0600

    added image controller script

---
+++ Shifting Sands/Assets/New Game Mechanics/ImageCtrlr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class ImageCtrlr : MonoBehaviour
+{
+    public Image imageComponent;
+    public FloatData ImgFillFloat;
+    private void Start ()
+    {
+        imageComponent = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        imageComponent.fillAmount = ImgFillFloat.value;
+    }
+
+}
